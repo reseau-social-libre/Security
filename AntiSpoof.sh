d394523fa88916ae6b460ac1d27f28680446cc83
root <root@vps-42889.fhnet.fr>
2019-02-24 21:50:30 +0000

    spoof

---
+++ AntiSpoof.sh
@@ -0,0 +1,21 @@
+NT_IF="eth0" # connected to internet
+IPT="/sbin/iptables" # path to iptables
+ 
+# default action, can be DROP or REJECT
+ACTION="DROP"
+ 
+# Drop packet that claiming from our own server on WAN port
+$IPT -A INPUT -i $INT_IF -s $SERVER_IP -j $ACTION
+$IPT -A OUTPUT -o $INT_IF -s $SERVER_IP -j $ACTION
+ 
+# Drop packet that claiming from our own internal LAN on WAN port
+$IPT -A INPUT -i $INT_IF -s $LAN_RANGE -j $ACTION
+$IPT -A OUTPUT -o $INT_IF -s $LAN_RANGE -j $ACTION
+ 
+## Drop all spoofed
+for ip in $SPOOF_IPS
+do
+ $IPT -A INPUT -i $INT_IF -s $ip -j $ACTION
+ $IPT -A OUTPUT -o $INT_IF -s $ip -j $ACTION
+done
+## add or call your rest of script below to customize iptables ##
